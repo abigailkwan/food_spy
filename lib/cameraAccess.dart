90120d6baec47647b51cc3b4773cd144510b6ce3
abigailkwan <60206977+abigailkwan@users.noreply.github.com>
2020-05-19 16:01:13 -0700

    added QoL changes for camera page

--- lib/cameraAccess.dart
+++ lib/cameraAccess.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 import 'services.dart';
 import 'text_style.dart';
 import 'picturebutton.dart';
+import 'package:intl/intl.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class cameraAccess extends StatefulWidget {
 
@@ -13,13 +15,40 @@ class cameraAccess extends StatefulWidget {
 
 class cameraAccessState extends State<cameraAccess> {
 
+  String imageKey;
+
+  String getDate() {
+    DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm:ss");
+    String formattedDate = dateFormat.format(DateTime.now());
+    return formattedDate;
+  }
+
+  @override
+  void initState(){
+    imageKey = getDate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         color: Color.fromRGBO(0, 66, 116, 1.0),
       child: ListView(
         children: <Widget>[
-          Image.network('https://abigailkwan.000webhostapp.com/img/abby.jpg'),
+          InkWell(
+            onTap: (){
+              setState(() {
+                imageKey = getDate();
+              });
+            },
+            child: Stack(
+              children: <Widget>[
+                Center(child: CircularProgressIndicator()),
+                Center(child: FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: 'https://abigailkwan.000webhostapp.com/img/abby.jpg' + "?v=" + imageKey)),
+              ]
+            ),
+          ),
           RaisedButton(
             child: Text("Add Picture to Profile"),
             onPressed: () {
